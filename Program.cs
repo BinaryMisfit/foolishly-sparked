bf7f5966e139703b86bb24ebcb5e39e4393130e1
Willie Roberts <diagoza@me.com>
2022-02-03 08:42:23 +0200

    Added new files and updated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sims.Toolkit
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
